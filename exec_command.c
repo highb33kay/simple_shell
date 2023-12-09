3ee5c40f46a5118302868741474d02c1b55a10b1
highb33kay <hiibeekayvibe@gmail.com>
2023-12-09 18:52:44 +0100

    test

--- exec_command.c
+++ exec_command.c
@@ -35,7 +35,9 @@ int execute_command(char **tokens)
 
 		printf("Child %d executing command: %s\n", getpid(), tokens[0]);
 
-		execvp(path, tokens);
+		tokens[0] = path;
+
+		execve(path, tokens, NULL);
 
 		fprintf(stderr, "Error: execve failed\n");
 		perror(tokens[0]);
